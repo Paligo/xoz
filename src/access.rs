dc115a4c44a0aa8b58a4fee2ae8b5f1128c9cc12
Martijn Faassen <faassen@startifact.com>
2024-12-16 18:56:52 +0100

    Analyze xot accessors.

---
+++ src/access.rs
@@ -0,0 +1,173 @@
+use crate::data::Structure;
+
+pub struct Xoz {
+    structure: Structure,
+}
+
+impl Xoz {
+    // document_element
+
+    // top_element
+
+    // root
+
+    // parent
+
+    // attributes
+
+    // get_attribute
+
+    // namespaces
+
+    // get_namespace
+
+    // attribute_nodes
+
+    // first_child
+
+    // last_child
+
+    // next_sibling
+
+    // previous_sibling
+
+    // ancestors
+
+    // children
+
+    // child_index
+
+    // reverse_children
+
+    // descendants
+
+    // all_descendants
+
+    // following_siblings
+
+    // preceding_siblings
+
+    // following
+
+    // preceding
+
+    // traverse
+
+    // all_traverse
+
+    // reverse_traverse
+
+    // level_order
+
+    // axis
+
+    // name
+
+    // name_ns
+
+    // namespace
+
+    // prefix
+
+    // no_namespace
+
+    // empty_prefix
+
+    // xml_prefix
+
+    // xml_namespace
+
+    // xml_id_name
+
+    // name_ref
+
+    // name_ns_str
+
+    // local_name_str
+
+    // uri_str
+
+    // namespace_str
+
+    // prefix_str
+
+    // namespace_for_name
+
+    // full_name
+
+    // node_name
+
+    // node_name_ref
+
+    // is_prefix_defined
+
+    // inherited_prefixes
+
+    // prefix_for_namespace
+
+    // namespace_for_prefix
+
+    // unresolved_namespaces
+
+    // namespaces_in_scope
+
+    // value
+
+    // value_type
+
+    // has_document_parent
+
+    // is_document_element
+
+    // is_document
+
+    // is_element
+
+    // is_text
+
+    // is_comment
+
+    // is_processing_instruction
+
+    // is_namespace_node
+
+    // is_attribute_node
+
+    // text (maybe)
+
+    // text_str
+
+    // get_element_name
+
+    // element
+
+    // text_content
+
+    // text_content_str
+
+    // comment
+
+    // comment_str
+
+    // processing_instruction
+
+    // namespace_node
+
+    // attribute_node
+
+    // string_value
+
+    // deep_equal
+
+    // deep_equal_children
+
+    // deep_equal_xpath
+
+    // advanced_deep_equal
+
+    // shallow_equal
+
+    // shallow_equal_ignore_attributes
+
+    // and parsing and serialization
+}
