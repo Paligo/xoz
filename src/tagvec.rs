7e4def0e76aada077bb88cfd85295cb5acd7d670
Martijn Faassen <faassen@startifact.com>
2024-12-17 13:48:41 +0100

    A couple more tests.

--- src/tagvec.rs
+++ src/tagvec.rs
@@ -12,11 +12,13 @@ pub(crate) trait TagVec {
     fn get_tag(&self, i: usize) -> Option<TagId>;
 
     /// Returns the number of occurrences of `tag_id` up to position `i`.
+    ///
+    /// If `i` is out of bounds, it returns `None`.
     fn rank_tag(&self, i: usize, tag_id: TagId) -> Option<usize>;
 
     /// Returns the position of the `rank`-th occurrence of `tag_id`, starting from `offset`.
     ///
-    /// If `offset` is out of bounds, or if the rankk-th occurrence of `tag_id` does not exist,
+    /// If `offset` is out of bounds, or if the rankkth occurrence of `tag_id` does not exist,
     /// it returns `None`
     fn select_tag(&self, offset: usize, rank: usize, tag_id: TagId) -> Option<usize>;
 }
@@ -54,6 +56,7 @@ mod tests {
         assert_eq!(wm.get_tag(0), Some(TagId::new(0)));
         assert_eq!(wm.get_tag(1), Some(TagId::new(1)));
         assert_eq!(wm.get_tag(2), Some(TagId::new(2)));
+        assert_eq!(wm.get_tag(10), None);
     }
 
     #[test]
@@ -64,5 +67,19 @@ mod tests {
         assert_eq!(wm.rank_tag(2, TagId::new(1)), Some(1));
         assert_eq!(wm.rank_tag(3, TagId::new(1)), Some(2));
         assert_eq!(wm.rank_tag(6, TagId::new(3)), Some(2));
+        assert_eq!(wm.rank_tag(10, TagId::new(3)), None);
+    }
+
+    #[test]
+    fn test_wm_select_tag() {
+        let wm = make_wavelet_matrix_usage(&[0, 1, 1, 3, 2, 3]).unwrap();
+        assert_eq!(wm.select_tag(0, 0, TagId::new(0)), Some(0));
+        assert_eq!(wm.select_tag(0, 1, TagId::new(0)), None);
+        assert_eq!(wm.select_tag(0, 0, TagId::new(1)), Some(1));
+        assert_eq!(wm.select_tag(0, 1, TagId::new(1)), Some(2));
+        assert_eq!(wm.select_tag(0, 0, TagId::new(3)), Some(3));
+        assert_eq!(wm.select_tag(0, 1, TagId::new(3)), Some(5));
+        assert_eq!(wm.select_tag(0, 2, TagId::new(3)), None);
+        assert_eq!(wm.select_tag(10, 0, TagId::new(3)), None);
     }
 }
