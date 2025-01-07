3a7fe6e82207eca8d02ab56fd1e42c97edb8dbcb
Martijn Faassen <faassen@startifact.com>
2025-01-07 14:08:43 +0100

    Add another test for multiple a.

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -10,7 +10,24 @@ fn test_elements() {
     let doc_el_name = doc.node_name(doc_el).unwrap();
     let a_name = doc.node_name(a).unwrap();
     let b_name = doc.node_name(b).unwrap();
+
     assert_eq!(doc_el_name.local_name(), "doc");
     assert_eq!(a_name.local_name(), "a");
     assert_eq!(b_name.local_name(), "b");
+}
+
+#[test]
+fn test_elements_multiple_a() {
+    let doc = parse_document(r#"<doc><a/><a/></doc>"#).unwrap();
+    let doc_el = doc.document_element();
+    let a1 = doc.first_child(doc_el).unwrap();
+    let a2 = doc.next_sibling(a1).unwrap();
+
+    let doc_el_name = doc.node_name(doc_el).unwrap();
+    let a1_name = doc.node_name(a1).unwrap();
+    let a2_name = doc.node_name(a2).unwrap();
+
+    assert_eq!(doc_el_name.local_name(), "doc");
+    assert_eq!(a1_name.local_name(), "a");
+    assert_eq!(a2_name.local_name(), "a");
 }
