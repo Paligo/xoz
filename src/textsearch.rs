177004db93e8d81ce27e489af4288e7fd67eca28
Martijn Faassen <faassen@startifact.com>
2025-01-10 13:10:47 +0100

    Add various more tests.

--- src/textsearch.rs
+++ src/textsearch.rs
@@ -45,12 +45,23 @@ impl TextSearch {
     }
 
     pub(crate) fn locate(&self, pattern: &str) -> Vec<usize> {
+        // a bit of duplication so we don't have to turn stuff into bytes and then
+        // back into a str in locate_by_bytes
         if self.is_tiny {
             // if it's tiny we can't use the text index, so just search directly
             return self.text.match_indices(pattern).map(|(i, _)| i).collect();
         }
+        self.locate_by_bytes(pattern.as_bytes())
+    }
+
+    pub(crate) fn locate_by_bytes(&self, pattern: &[u8]) -> Vec<usize> {
+        if self.is_tiny {
+            let pattern = std::str::from_utf8(pattern).unwrap();
+            // if it's tiny we can't use the text index, so just search directly
+            return self.text.match_indices(pattern).map(|(i, _)| i).collect();
+        }
         self.index
-            .search_backward(pattern.as_bytes())
+            .search_backward(pattern)
             .locate()
             .iter()
             .map(|i| {
@@ -66,9 +77,11 @@ impl TextSearch {
         // TODO: an alternative implementation would look for \0pattern, but
         // would need special handling for the first section, plus allocation,
         // so not sure it's faster
+        // if we *could* have the text start with 0 (but fm index right now crashes on it),
+        // it would actually make code a lot simpler.
         self.locate(pattern)
             .into_iter()
-            .filter(|&i| i == 0 || dbg!(self.text.as_bytes()[i - 1]) == 0)
+            .filter(|&i| i == 0 || self.text.as_bytes()[i - 1] == 0)
             .collect()
     }
 
@@ -81,12 +94,11 @@ impl TextSearch {
     }
 
     pub(crate) fn equals(&self, pattern: &str) -> Vec<usize> {
-        // find those text indices that are surrounded by \0
         self.locate(pattern)
             .into_iter()
             .filter(|&i| {
                 let bytes = self.text.as_bytes();
-                (i == 0 || bytes[i - 1] == 0) && bytes[i + pattern.len()] == 0
+                bytes[i + pattern.len()] == 0 && (i == 0 || bytes[i - 1] == 0)
             })
             .collect()
     }
@@ -132,7 +144,7 @@ mod tests {
 
     #[test]
     fn test_starts_with_beginning() {
-        let text = "hello\0world hello\0";
+        let text = "hello something\0world hello\0";
         let search = TextSearch::new(text.to_string());
         let mut located = search.starts_with("hello");
         located.sort();
@@ -143,9 +155,58 @@ mod tests {
     fn test_starts_with_middle() {
         let text = "world\0hello world\0";
         let search = TextSearch::new(text.to_string());
-        dbg!(search.locate("hello"));
         let mut located = search.starts_with("hello");
         located.sort();
         assert_eq!(located, vec![6]);
+    }
+
+    #[test]
+    fn test_ends_with_simple() {
+        let text = "hello world\0";
+        let search = TextSearch::new(text.to_string());
+        let mut located = search.ends_with("world");
+        located.sort();
+        assert_eq!(located, vec![6]);
+    }
+
+    #[test]
+    fn test_doesnt_end_with() {
+        let text = "hello world\0";
+        let search = TextSearch::new(text.to_string());
+        let located = search.ends_with("hello");
+        assert_eq!(located, vec![]);
+    }
+
+    #[test]
+    fn test_ends_with_middle() {
+        let text = "world\0hello world\0";
+        let search = TextSearch::new(text.to_string());
+        let mut located = search.ends_with("world");
+        located.sort();
+        assert_eq!(located, vec![0, 12]);
+    }
+
+    #[test]
+    fn test_equals() {
+        let text = "hello\0";
+        let search = TextSearch::new(text.to_string());
+        let mut located = search.equals("hello");
+        located.sort();
+        assert_eq!(located, vec![0]);
+        let mut located = search.equals("hel");
+        located.sort();
+        assert_eq!(located, vec![]);
+    }
+
+    #[test]
+    fn test_equals_middle() {
+        let text = "hello\0world\0";
+        let search = TextSearch::new(text.to_string());
+        let mut located = search.equals("world");
+        located.sort();
+        assert_eq!(located, vec![6]);
+        let mut located = search.equals("wor");
+        located.sort();
+        assert_eq!(located, vec![]);
     }
 }
